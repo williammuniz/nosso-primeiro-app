e895e395a5214d9eb9fe4ba26f5d9916cf6773a7
William <william.muz@gmail.com>
2018-09-05 22:24:59 -0300

    Consumindo api do randomuser

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import Header from './src/components/Header';
+import axios from 'axios';
 
 export default class App extends React.Component {
+    renderList() {
+        // const textElements = names.map((name, index) => {
+        //     return <Text key={index}>{name}</Text>
+        // });
+        axios.get('https://randomuser.me/api/?nat=br&results=5')
+            .then(response => {
+                const {results} = response.data;
+                const names = results.map(people => people.name.first);
+                console.log(names);
+            });
+        // return textElements;
+    }
     render() {
         return (
             <View>
                 <Header title="Pessoas!"/>
+                {this.renderList()}
             </View>
         );
     }
