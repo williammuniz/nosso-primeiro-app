af28d46d0031c535dfb86fb4ad2139abb64fa456
William <william.muz@gmail.com>
2018-09-05 21:39:30 -0300

    Criando componente de header

--- App.js
+++ App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import Header from './src/components/Header';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Opens ddsddsup App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+    render() {
+        return (
+            <View >
+                <Header/>
+            </View>
 
-    );
-  }
+        );
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
