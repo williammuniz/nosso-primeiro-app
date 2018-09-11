31d0ec7217966293cd3f70a5b98c51b0db69baf2
William <william.muz@gmail.com>
2018-09-10 22:17:32 -0300

    Criando a lista de pessoas, separando o css para cada responsável pelo style, colocando imagem na lista das pessoas utilizando o Image do react

--- App.js
+++ App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Text, View} from 'react-native';
 import Header from './src/components/Header';
+import PeopleList from './src/components/PeopleList';
 import axios from 'axios';
 
 export default class App extends React.Component {
-    renderList() {
-        // const textElements = names.map((name, index) => {
-        //     return <Text key={index}>{name}</Text>
-        // });
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            peoples: []
+        };
+    }
+
+    componentDidMount() {
         axios.get('https://randomuser.me/api/?nat=br&results=5')
             .then(response => {
                 const {results} = response.data;
-                const names = results.map(people => people.name.first);
-                console.log(names);
-            });
-        // return textElements;
+                this.setState({
+                    peoples: results
+                });
+            })
     }
+
     render() {
         return (
             <View>
                 <Header title="Pessoas!"/>
-                {this.renderList()}
+                <PeopleList peoples={this.state.peoples}/>
             </View>
         );
     }
